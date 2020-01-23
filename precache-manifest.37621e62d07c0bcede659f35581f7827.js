85caa83aa751f788b7225a9f6b7bcc120728bdf2
evelyn.graumann <evelyn.graumann@crosslend.com>
2020-01-23 14:43:04 +0100

    Updates

---
+++ precache-manifest.37621e62d07c0bcede659f35581f7827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14681842f2566b25deb63cf6ad0cc8c9",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "4bf564d64a4ef823d58c",
+    "url": "/react-calculator/static/css/main.a5b162f3.chunk.css"
+  },
+  {
+    "revision": "eae4984137b3b9ea3e54",
+    "url": "/react-calculator/static/js/2.398528b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calculator/static/js/2.398528b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bf564d64a4ef823d58c",
+    "url": "/react-calculator/static/js/main.59b89dbc.chunk.js"
+  },
+  {
+    "revision": "cea85ed20600dac0e94f",
+    "url": "/react-calculator/static/js/runtime-main.84ab1775.js"
+  }
+]);
